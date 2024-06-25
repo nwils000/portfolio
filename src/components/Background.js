d94b1e499a1f6c47de758c46e7988e6169556f34
nwils000 <nwils000@gmail.com>
2024-06-25 13:29:59 -0400

    took out background animations

--- src/components/Background.js
+++ src/components/Background.js
@@ -14,150 +14,150 @@ const getNumShapes = () => {
 
 const NUM_SHAPES = getNumShapes();
 
-const getRandomPosition = (size) => {
-  let maxX = 1000;
-  let maxY = 1000;
-  if (typeof window !== 'undefined') {
-    maxX = window.innerWidth;
-    maxY = window.innerHeight;
-  }
-  const x = Math.max(0, Math.floor(Math.random() * (maxX - size)));
-  const y = Math.max(0, Math.floor(Math.random() * (maxY - size)));
-  return { x, y };
-};
+// const getRandomPosition = (size) => {
+//   let maxX = 1000;
+//   let maxY = 1000;
+//   if (typeof window !== 'undefined') {
+//     maxX = window.innerWidth;
+//     maxY = window.innerHeight;
+//   }
+//   const x = Math.max(0, Math.floor(Math.random() * (maxX - size)));
+//   const y = Math.max(0, Math.floor(Math.random() * (maxY - size)));
+//   return { x, y };
+// };
 
-const getRandomVelocity = () => {
-  return Math.random() * 10 - 5;
-};
+// const getRandomVelocity = () => {
+//   return Math.random() * 10 - 5;
+// };
 
-const getRandomOpacity = () => {
-  return 0.3 + Math.random() * 0.7;
-};
+// const getRandomOpacity = () => {
+//   return 0.3 + Math.random() * 0.7;
+// };
 
-const Square = ({ backgroundColor, size, speed }) => {
-  const [squareState, setSquareState] = useState({
-    position: getRandomPosition(size),
-    velocity: { x: getRandomVelocity(), y: getRandomVelocity() },
-    opacity: getRandomOpacity(),
-  });
-  const requestRef = useRef();
+// const Square = ({ backgroundColor, size, speed }) => {
+//   const [squareState, setSquareState] = useState({
+//     position: getRandomPosition(size),
+//     velocity: { x: getRandomVelocity(), y: getRandomVelocity() },
+//     opacity: getRandomOpacity(),
+//   });
+//   const requestRef = useRef();
 
-  const updatePosition = () => {
-    const { position, velocity } = squareState;
-    const newX = position.x + velocity.x * speed;
-    const newY = position.y + velocity.y * speed;
-    const maxX = window.innerWidth - size;
-    const maxY = window.innerHeight - size;
+//   const updatePosition = () => {
+//     const { position, velocity } = squareState;
+//     const newX = position.x + velocity.x * speed;
+//     const newY = position.y + velocity.y * speed;
+//     const maxX = window.innerWidth - size;
+//     const maxY = window.innerHeight - size;
 
-    const newVelocity = { ...velocity };
-    if (newX < 0 || newX > maxX) {
-      newVelocity.x *= -1;
-    }
-    if (newY < 0 || newY > maxY) {
-      newVelocity.y *= -1;
-    }
+//     const newVelocity = { ...velocity };
+//     if (newX < 0 || newX > maxX) {
+//       newVelocity.x *= -1;
+//     }
+//     if (newY < 0 || newY > maxY) {
+//       newVelocity.y *= -1;
+//     }
 
-    setSquareState((prevState) => ({
-      position: { x: newX, y: newY },
-      velocity: newVelocity,
-      opacity: prevState.opacity,
-    }));
-  };
+//     setSquareState((prevState) => ({
+//       position: { x: newX, y: newY },
+//       velocity: newVelocity,
+//       opacity: prevState.opacity,
+//     }));
+//   };
 
-  useEffect(() => {
-    const animate = () => {
-      updatePosition();
-      requestRef.current = requestAnimationFrame(animate);
-    };
+// useEffect(() => {
+//   const animate = () => {
+//     updatePosition();
+//     requestRef.current = requestAnimationFrame(animate);
+//   };
 
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, [squareState, size, speed]);
+//   requestRef.current = requestAnimationFrame(animate);
+//   return () => cancelAnimationFrame(requestRef.current);
+// }, [squareState, size, speed]);
 
-  return (
-    <svg
-      width={size}
-      height={size}
-      className="absolute top-0 left-0 bg-purple-600 border-none"
-      style={{
-        top: squareState.position.y,
-        left: squareState.position.x,
-        opacity: squareState.opacity,
-        transition: 'opacity 0.5s',
-      }}
-    >
-      <rect className="fill-none" x="0" y="0" width={size} height={size} />
-    </svg>
-  );
-};
+// return (
+//   <svg
+//     width={size}
+//     height={size}
+//     className="absolute top-0 left-0 bg-purple-600 border-none"
+//     style={{
+//       top: squareState.position.y,
+//       left: squareState.position.x,
+//       opacity: squareState.opacity,
+//       transition: 'opacity 0.5s',
+//     }}
+//   >
+//     <rect className="fill-none" x="0" y="0" width={size} height={size} />
+//   </svg>
+// );
+// };
 
-const Slider = ({ label, min, max, step, value, onChange }) => {
-  const handleSliderChange = (event) => {
-    const newValue = Number(event.target.value);
-    onChange(newValue);
-  };
+// const Slider = ({ label, min, max, step, value, onChange }) => {
+//   const handleSliderChange = (event) => {
+//     const newValue = Number(event.target.value);
+//     onChange(newValue);
+//   };
 
-  return (
-    <div className="p-4">
-      <label className="block mb-2 text-sm font-medium text-gray-400">
-        {label}
-      </label>
-      <input
-        type="range"
-        min={min}
-        max={max}
-        step={step}
-        value={value}
-        onChange={handleSliderChange}
-        className="w-full h-8 rounded-full outline-none appearance-none bg-gradient-to-r from-gray-700 to-gray-800 opacity-70 hover:opacity-100 slider-horizontal"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div className="p-4">
+//       <label className="block mb-2 text-sm font-medium text-gray-400">
+//         {label}
+//       </label>
+//       <input
+//         type="range"
+//         min={min}
+//         max={max}
+//         step={step}
+//         value={value}
+//         onChange={handleSliderChange}
+//         className="w-full h-8 rounded-full outline-none appearance-none bg-gradient-to-r from-gray-700 to-gray-800 opacity-70 hover:opacity-100 slider-horizontal"
+//       />
+//     </div>
+//   );
+// };
 
 const Background = () => {
-  const [squareSize, setSquareSize] = useState(5);
-  const [squareSpeed, setSquareSpeed] = useState(0.5);
-  const [squareAmount, setSquareAmount] = useState(NUM_SHAPES);
+  // const [squareSize, setSquareSize] = useState(5);
+  // const [squareSpeed, setSquareSpeed] = useState(0.5);
+  // const [squareAmount, setSquareAmount] = useState(NUM_SHAPES);
 
-  const [windowWidth, setWindowWidth] = useState(0);
+  // const [windowWidth, setWindowWidth] = useState(0);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setWindowWidth(window.innerWidth);
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     setWindowWidth(window.innerWidth);
 
-      const handleResize = () => {
-        setWindowWidth(window.innerWidth);
-      };
+  //     const handleResize = () => {
+  //       setWindowWidth(window.innerWidth);
+  //     };
 
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, []);
+  //     window.addEventListener('resize', handleResize);
+  //     return () => window.removeEventListener('resize', handleResize);
+  //   }
+  // }, []);
 
-  const handleSizeChange = (newValue) => {
-    setSquareSize(newValue);
-  };
+  // const handleSizeChange = (newValue) => {
+  //   setSquareSize(newValue);
+  // };
 
-  const handleSpeedChange = (newValue) => {
-    setSquareSpeed(newValue);
-  };
+  // const handleSpeedChange = (newValue) => {
+  //   setSquareSpeed(newValue);
+  // };
 
-  const handleAmountChange = (newValue) => {
-    setSquareAmount(newValue);
-  };
+  // const handleAmountChange = (newValue) => {
+  //   setSquareAmount(newValue);
+  // };
 
-  const squares =
-    windowWidth > 767
-      ? Array.from({ length: squareAmount }).map((_, index) => (
-          <Square
-            key={index}
-            backgroundColor="purple"
-            size={squareSize}
-            speed={squareSpeed}
-          />
-        ))
-      : null;
+  // const squares =
+  //   windowWidth > 767
+  //     ? Array.from({ length: squareAmount }).map((_, index) => (
+  //         <Square
+  //           key={index}
+  //           backgroundColor="purple"
+  //           size={squareSize}
+  //           speed={squareSpeed}
+  //         />
+  //       ))
+  //     : null;
 
   return (
     <>
